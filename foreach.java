ebaae8494c23bc08aa6fbea5ff61cc064c842478
sha112419 <104012644+sha112419@users.noreply.github.com>
2022-05-12 14:03:57 +0530

    Add files via upload

---
+++ foreach.java
@@ -0,0 +1,13 @@
+package sha;
+
+public class foreach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int s[]= {11,24,19,2};
+for(int f:s) {
+	System.out.println(f);
+}
+	}
+
+}
